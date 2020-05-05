f225817b5b6385aac185a8a5189d4854fd1e75c8
FernandoCendrettiSolucx <fernando.cendretti@solucx.com.br>
2020-05-05 14:08:12 -0300

    Validation datas in Update User

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -45,6 +45,23 @@ class UserController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
     const { oldPassword } = req.body;
 
     const user = await User.findByPk(req.userId);
